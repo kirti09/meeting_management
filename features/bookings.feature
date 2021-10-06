f4a9a2ff74f62e9eb4a4e1211906a947f84352a1
Syed Tabrez <syedtabrez123@gmail.com>
2021-10-06 20:53:40 +0530

    corrections based on review

--- features/bookings.feature
+++ features/bookings.feature
@@ -8,16 +8,16 @@ Feature: Testing meeting room booking API
   Scenario: Booking an available meeting room
     Given A meeting room is "available"
     When I make a POST api request to endpoint "/v1/bookings" with below params:
-      | start_time | 2099-09-29 10:00:00 |
-      | end_time   | 2099-09-29 10:30:00 |
+      | start_time | end_time | meeting_room_id |
+      | 2099-09-29 10:00:00 | 2099-09-29 10:30:00 | 1 |
     Then my booking is made successfully
     And the response code is "201"
 
   Scenario: Booking an unavailable meeting room
     Given A meeting room is "unavailable"
     When I make a POST api request to endpoint "/v1/bookings" with below params:
-      | start_time | 2099-09-29 10:00:00 |
-      | end_time   | 2099-09-29 10:30:00 |
+      | start_time | end_time | meeting_room_id |
+      | 2099-09-29 10:00:00 | 2099-09-29 10:30:00 | 1 |
     Then I receive the below error response:
     """
     {"meeting_room_id":["Meeting Room not available for booking."]}
@@ -26,14 +26,10 @@ Feature: Testing meeting room booking API
 
   Scenario: Booking an unavailable time slot for a meeting room
     Given A meeting room is "available"
+    And the time slot "2099-09-29 10:00:00" to "2099-09-29 10:30:00" is already booked
     When I make a POST api request to endpoint "/v1/bookings" with below params:
-      | start_time | 2099-09-29 10:00:00 |
-      | end_time   | 2099-09-29 10:30:00 |
-    Then my booking is made successfully
-    And the response code is "201"
-    When I repeat the POST api request to endpoint "/v1/bookings" with same params:
-      | start_time | 2099-09-29 10:00:00 |
-      | end_time   | 2099-09-29 10:30:00 |
+      | start_time | end_time | meeting_room_id |
+      | 2099-09-29 10:00:00 | 2099-09-29 10:30:00 | 1 |
     Then I receive the below error response:
     """
     {"start_time":["Requested time slot not available."]}
--- features/step_definitions/bookings_steps.rb
+++ features/step_definitions/bookings_steps.rb
@@ -1,26 +1,24 @@
 # frozen_string_literal: true
 
 Given(/^I am a registered user$/) do
-  @user = User.create(first_name: 'user', last_name: 'one', email: 'user1@test.com')
+  @user = FactoryBot.create(:user)
 end
 
 Given(/^A meeting room is "(.*)"$/) do |availability|
-  @meeting_room = if availability == 'available'
-                    MeetingRoom.create(name: 'MR1', capacity: 10, is_available: true)
-                  else
-                    MeetingRoom.create(name: 'MR1', capacity: 15, is_available: false)
-                  end
+  availability == 'available' ? FactoryBot.create(:meeting_room) : FactoryBot.create(:meeting_room, is_available: false)
 end
 
 When(/^I make a POST api request to endpoint "(.*)" with below params:$/) do |url, table|
-  @payload = table.rows_hash.merge!(user_id: @user.id, meeting_room_id: @meeting_room.id)
-  page.driver.post(url, @payload)
+  table.hashes.each do |data|
+    @payload = data.merge!(user_id: @user.id)
+    page.driver.post(url, @payload)
+  end
 end
 
 Then(/^my booking is made successfully$/) do
   booking = Booking.last
   expect(booking.user_id).to eq(@payload[:user_id])
-  expect(booking.meeting_room_id).to eq(@payload[:meeting_room_id])
+  expect(booking.meeting_room_id).to eq(@payload[:meeting_room_id].to_i)
 end
 
 And(/^the response code is "(.*)"$/) do |http_code|
@@ -31,7 +29,7 @@ Then(/^I receive the below error response:$/) do |error_response|
   expect(JSON.parse(page.body)).to eq(JSON.parse(error_response))
 end
 
-When(/^I repeat the POST api request to endpoint "(.*)" with same params:$/) do |url, table|
-  @payload = table.rows_hash.merge!(user_id: @user.id, meeting_room_id: @meeting_room.id)
-  page.driver.post(url, @payload)
+And(/^the time slot "(.*)" to "(.*)" is already booked$/) do |start_time, end_time|
+  @user.bookings.create(start_time: start_time.to_datetime, end_time: end_time.to_datetime,
+                        meeting_room: MeetingRoom.first)
 end
